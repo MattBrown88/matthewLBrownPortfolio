c3b9bc3a7138bff7bffc09166cb90e1536026d80
mattbrown88 <mattbrown7531@gmail.com>
2026-07-20 10:53:14 -0500

    DAd

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,4 @@
-(function () {
+function loadAnalytics() {
   var s = document.createElement('script');
   s.async = true;
   s.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZRGR3F5RGX';
@@ -9,4 +9,15 @@
   window.gtag = gtag;
   gtag('js', new Date());
   gtag('config', 'G-ZRGR3F5RGX');
-})();
+
+  var sb = document.createElement('script');
+  sb.async = true;
+  sb.src = 'https://cdn.splitbee.io/sb.js';
+  document.head.appendChild(sb);
+}
+
+window.loadAnalytics = loadAnalytics;
+
+if (localStorage.getItem('cookie-consent') === 'accepted') {
+  loadAnalytics();
+}
